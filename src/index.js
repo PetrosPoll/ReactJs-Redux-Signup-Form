cc507c2443901ffcaa01e3200e3686df4ccc54d6
Peter Pollakis <40791424+PeterPoll@users.noreply.github.com>
2021-09-28 23:15:14 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,14 +4,19 @@ import App from './components/signup_process';
 import { Provider } from 'react-redux';
 import store from './store/store';
 import { StyledEngineProvider } from '@mui/material/styles';
+import DateFnsUtils from '@date-io/date-fns';
+import {
+  MuiPickersUtilsProvider,
+} from '@material-ui/pickers';
+
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <StyledEngineProvider injectFirst>
-
+        <MuiPickersUtilsProvider utils={DateFnsUtils}>
           <App />
-
+          </MuiPickersUtilsProvider>
       </StyledEngineProvider>
     </Provider>
   </React.StrictMode>,
